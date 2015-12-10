618854d53f2215f764561b02b235b56df7a27f17
= <halldis13@ru.is>
2015-12-10 01:35:09 +0000

    deploy skrifta komin

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-
+docker push halldis13/tictactoe
+ssh vagrant@192.168.50.4 <<-'ENDSSH'
+docker pull halldis13/tictactoe
+docker kill testcontainer
+docker rm testcontainer
+docker run --name testcontainer -p 8080:8080 -d -e "NODE_ENV=production" halldis13/tictactoe
+ENDSSH
